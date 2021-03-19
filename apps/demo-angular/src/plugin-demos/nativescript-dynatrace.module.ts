1c32f1f08099d71d6f284e2cd4eb9001edcf8fd2
triniwiz <fortune.osei@yahoo.com>
2021-03-19 11:56:29 -0400

    fix: rename

--- apps/demo-angular/src/plugin-demos/nativescript-dynatrace.module.ts
+++ apps/demo-angular/src/plugin-demos/nativescript-dynatrace.module.ts
@@ -1,10 +1,10 @@
 import { NgModule, NO_ERRORS_SCHEMA } from '@angular/core';
 import { NativeScriptCommonModule, NativeScriptRouterModule } from '@nativescript/angular';
-import { DynatraceComponent } from './dynatrace.component';
+import { DynatraceComponent } from './nativescript-dynatrace.component';
 
 @NgModule({
 	imports: [NativeScriptCommonModule, NativeScriptRouterModule.forChild([{ path: '', component: DynatraceComponent }])],
 	declarations: [DynatraceComponent],
 	schemas: [NO_ERRORS_SCHEMA],
 })
-export class DynatraceModule {}
+export class NativescriptDynatraceModule {}
