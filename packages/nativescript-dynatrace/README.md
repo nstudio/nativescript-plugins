8ed587384169888ea67905e009ba3e623c1276fc
Nathan Walker <walkerrunpdx@gmail.com>
2021-04-10 12:40:52 -0700

    fix(dynatrace): build and setup

--- packages/nativescript-dynatrace/README.md
+++ packages/nativescript-dynatrace/README.md
@@ -1,7 +1,7 @@
-# @nstudio/dynatrace
+# @nstudio/nativescript-dynatrace
 
 ```bash
-ns plugin add @nstudio/dynatrace
+ns plugin add @nstudio/nativescript-dynatrace
 ```
 
 ## Usage
--- packages/nativescript-dynatrace/references.d.ts
+++ packages/nativescript-dynatrace/references.d.ts
@@ -1,1 +1,4 @@
+/// <reference path="../../node_modules/@nativescript/core/global-types.d.ts" />
 /// <reference path="../../references.d.ts" />
+/// <reference path="./typings/android.d.ts" />
+/// <reference path="./typings/objc!Dynatrace.d.ts" />
