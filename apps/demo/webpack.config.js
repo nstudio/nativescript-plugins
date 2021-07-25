f8451555cb9cfc947c43b8250738342b0e169513
Nathan Walker <walkerrunpdx@gmail.com>
2021-07-24 21:22:19 -0700

    chore: cleanup

--- apps/demo/webpack.config.js
+++ apps/demo/webpack.config.js
@@ -11,6 +11,7 @@ module.exports = (env) => {
     config.resolve.alias.set('@demo/shared', resolve(__dirname, '..', '..', 'tools', 'demo'));
   });
 
+  // Example if you need to share images across demo apps:
   // webpack.Utils.addCopyRule({
   //   from: '../../../tools/images', 
 	// 	to: 'images',
