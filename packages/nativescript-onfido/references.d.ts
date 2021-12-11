8f68592823c337357619cdf30669d96d0d07eb61
Nathan Walker <walkerrunpdx@gmail.com>
2021-12-11 10:20:00 -0800

    feat(onfido): android sdk 10.3.1

--- packages/nativescript-onfido/references.d.ts
+++ packages/nativescript-onfido/references.d.ts
@@ -1,2 +1,3 @@
 /// <reference path="../../references.d.ts" />
-/// <reference path="typings/objc!Onfido.d.ts" />
+/// <reference path="./typings/objc!Onfido.d.ts" />
+/// <reference path="./typings/android-onfido-capture-sdk.d.ts" />
