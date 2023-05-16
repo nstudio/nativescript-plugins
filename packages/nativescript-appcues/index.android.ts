73e6b12904e7761a313be1cd7b5fd5ad1a42a486
Osei Fortune <fortune.osei@yahoo.com>
2023-05-16 12:29:17 -0400

    fix(appcues): use kotlin help for constructor

--- packages/nativescript-appcues/index.android.ts
+++ packages/nativescript-appcues/index.android.ts
@@ -2,20 +2,11 @@ import { Utils } from '@nativescript/core';
 import { AppcuesSDKCommon } from './common';
 
 let appcuesInstance: com.appcues.Appcues;
+declare const io;
 
 export class AppcuesSDK extends AppcuesSDKCommon {
     static init(accountId: string, appId: string) {
-        // console.log('org.koin:', org.koin)
-        // for (const key in org.koin) {
-        //     console.log(key)
-        // }
-        // console.log('---')
-        // console.dir(org.koin.core.scope.Scope);
-        // const koin = new org.koin.core.Koin();
-        // koin.createScope()
-        // org.koin.core.scope.Scope
-        // appcuesInstance = new com.appcues.Appcues(Utils.android.getApplicationContext());//, accountId, appId)
-        console.log('Android TODO. Need Osei help on constructor with Koin.Scope.')
+        appcuesInstance = new io.nstudio.plugins.appcues.Appcues.init(Utils.android.getApplicationContext(), accountId, appId);
     }
 
     static identify(userId: string, properties?: any) {
