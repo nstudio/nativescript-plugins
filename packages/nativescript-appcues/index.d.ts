d7571f96e7adbe4a6a8321c63e23b4152a63eb09
Nathan Walker <walkerrunpdx@gmail.com>
2023-06-04 18:20:26 -0700

    feat: cleanup Appcues

--- packages/nativescript-appcues/index.d.ts
+++ packages/nativescript-appcues/index.d.ts
@@ -1,9 +1,8 @@
 import { AppcuesSDKCommon } from './common';
 
 export declare class AppcuesSDK extends AppcuesSDKCommon {
-    static init(accountId: string, appId: string): void;
     static nativeInstance(): any /* iOS or Android SDK instance */
-
+    static init(accountId: string, appId: string): void;
     static identify(userId: string, properties?: any): void;
     static track(name: string, properties?: any): void;
     static trackScreens(): void;
--- tools/demo/nativescript-appcues/index.ts
+++ tools/demo/nativescript-appcues/index.ts
@@ -4,12 +4,18 @@ import { AppcuesSDK } from '@nstudio/nativescript-appcues';
 export class DemoSharedNativescriptAppcues extends DemoSharedBase {
 
   init() {
-    AppcuesSDK.init('123', '123');
+    AppcuesSDK.init('<account-id>', '<app-id>');
   }
 
   track() {
     AppcuesSDK.track('hello', {
       name: 'Test'
     });
+  }
+
+  identify() {
+    AppcuesSDK.identify('789', {
+      name: 'Testing Demo'
+    })
   }
 }
