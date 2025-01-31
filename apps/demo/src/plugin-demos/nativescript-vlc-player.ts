d67d39ce9557d6516734663784240f3a758289ac
Nathan Walker <walkerrunpdx@gmail.com>
2025-01-30 20:49:57 -0800

    chore: demo

--- apps/demo/src/plugin-demos/nativescript-vlc-player.ts
+++ apps/demo/src/plugin-demos/nativescript-vlc-player.ts
@@ -1,10 +1,22 @@
 import { Observable, EventData, Page } from '@nativescript/core';
 import { DemoSharedNativescriptVlcPlayer } from '@demo/shared';
-import {} from '@nstudio/nativescript-vlc-player';
+import { VLCPlayer } from '@nstudio/nativescript-vlc-player';
 
 export function navigatingTo(args: EventData) {
 	const page = <Page>args.object;
 	page.bindingContext = new DemoModel();
 }
 
-export class DemoModel extends DemoSharedNativescriptVlcPlayer {}
+export class DemoModel extends DemoSharedNativescriptVlcPlayer {
+	video: VLCPlayer;
+	loaded(args) {
+		this.video = args.object;
+	}
+	play() {
+		this.video.play();
+	}
+
+	pause() {
+		this.video.pause();
+	}
+}
